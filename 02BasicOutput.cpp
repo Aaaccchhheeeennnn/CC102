3dcfde70a19abd37eb1c1eaf5dc8b84f15612f41
Francine May Gonzales <francinemay0714@gmail.com>
2024-10-07 08:52:13 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout<< "School\t\t\t	:	Our Lady of Lourdes College Foundation" << endl;
+	cout<< "Department\t\t\t:	OLLCF College of Computer Studies" << endl;
+	cout<< "Course & Year Level\t\t:	BSIT 1-C " << endl;
+	cout<< "Full Name\t\t	:	Francine May S. Gonzales " << endl;
+	cout<< "Address\t\t\t	:	P1, Alawihao, Daet, Camarines Norte" << endl;
+	
+return 0;
+}
